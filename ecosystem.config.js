f7dcb03512a5da8ce8dae93751d020b9054761ab
Slayer <aksuplayz1337@gmail.com>
2021-09-11 20:51:05 +0300

    Initial commit

---
+++ ecosystem.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [{
+    name: "SSHListener",
+    script: "./main.py",
+    interpreter: 'python3'
+  }]
+};
---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import socket
+import configparser
+
+url = 'https://api.abuseipdb.com/api/v2/report'
+
+config = configparser.ConfigParser()
+
+socketname = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socketname.bind(("", 22))
+socketname.listen()
+
+while True:
+  connection, address = socketname.accept()  # address is the ip
+  address = str(address).split("'")
+  address = address[1]
+
+  params = {
+      'ip': str(address),
+      'categories': '18,20',
+      'comment': "Unauthorized connection attempt detected from IP address " + str(address) + " to port 22"
+  }
+
+  headers = {
+      'Accept': 'application/json',
+      'Key': config['AbuseIPDB']['Key']
+  }
+
+  response = requests.request(
+      method='POST', url=url, params=params, headers=headers)
+
+  discord_webhook_url = config['Discord']['WebhookURL']
+
+  message = {
+      "content": "Attempted SSH Login From IP Address: " + str(address) + " Automatically Reporting To AbuseIPDB.com."
+  }
+
+  requests.post(discord_webhook_url, data=message)
+
+  print(response.status_code)
+  if response.status_code == 429:
+      print("IP Already reported - You must wait 15 minutes")
+  else:
+      pass
+  print("User reported, IP: " + str(address))
+  connection.close()
