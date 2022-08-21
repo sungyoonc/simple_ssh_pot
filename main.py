7082bef4a4ebcc5243c1d6964a20a49fc203e0e6
Slayer <aksuplayz1337@gmail.com>
2022-08-21 17:59:40 +0300

    Add 15min cache to reported ips

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import requests
-import socket
-import configparser
-import random
-import string
+import requests, socket, configparser, random, string
+
+from cachetools import TTLCache
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+try:
+  with open('config.ini') as f:
+    config.read(f)
+except IOError:
+    raise Exception('config.ini file was not found.')
+
+if not config['AbuseIPDB']['Key']:
+  raise Exception("Missing AbuseIPDB Key from config.ini, can not continue.")
+
 url = config['AbuseIPDB']['ReportURL']
+discord_webhook_url = config['Discord']['WebhookURL']
+
+cache = TTLCache(maxsize=50, ttl=900)
 
 socketname = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 socketname.bind(("", 22))
 socketname.listen()
+
+
+print('[INFO] ListenSSH is running')
 
 while True:
   connection, address = socketname.accept()  # address is the ip
@@ -30,21 +42,26 @@ while True:
       'Key': config['AbuseIPDB']['Key']
   }
 
-  response = requests.request(
-      method='POST', url=url, params=params, headers=headers)
-
-  discord_webhook_url = config['Discord']['WebhookURL']
-
   message = {
       "content": "Attempted SSH Login From IP Address: " + str(address) + " Automatically Reporting To AbuseIPDB.com."
   }
 
-  requests.post(discord_webhook_url, data=message)
+  if discord_webhook_url:
+    requests.post(discord_webhook_url, data=message)
 
-  print(response.status_code)
-  if response.status_code == 429:
-      print("IP Already reported - You must wait 15 minutes")
+  if cache.get(str(address), None) != True:
+    response = requests.request(method='POST', url=url, params=params, headers=headers)
+
+    if response.status_code == 429:
+        cache[str(address)] = True
+        print("IP Already reported - You must wait 15 minutes")
+    else:
+        cache[str(address)] = True
+        print("User reported, IP: " + str(address))
+        pass
   else:
-      pass
-  print("User reported, IP: " + str(address))
+    print('[INFO] Cache: ip exists in cache (TTL: 15mins)')
+
+
+    
   connection.close()
